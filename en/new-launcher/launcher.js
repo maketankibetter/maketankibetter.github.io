b3747a06cbca21f6532dd6eac77239b5589b7961
yaroslav.vyazmitin <yaroslav.vyazmitin@gmail.com>
2024-05-24 11:58:45 +0000

    beta branch v13 - big UX update & bugfixes

--- en/new-launcher/launcher.js
+++ en/new-launcher/launcher.js
@@ -18,7 +18,7 @@ const serverdata = [["https://balancer.eu.tankionline.com/balancer/",           
                     ["https://balancer.review-7-public.test-ru.tankionline.com/balancer", "review7", "https://client-review-7-public.test-ru.tankionline.com/?config-template=https://c{server}.review-7-public.test-ru.tankionline.com/config.xml&resources=https://resources-review-7-public.test-ru.tankionline.com&balancer=https://balancer.review-7-public.test-ru.tankionline.com/balancer", "Client Review #7"],
                     ["https://balancer.review-8-public.test-ru.tankionline.com/balancer", "review8", "https://client-review-8-public.test-ru.tankionline.com/?config-template=https://c{server}.review-8-public.test-ru.tankionline.com/config.xml&resources=https://resources-review-8-public.test-ru.tankionline.com&balancer=https://balancer.review-8-public.test-ru.tankionline.com/balancer", "Client Review #8"],
                     ["https://balancer.review-9-public.test-ru.tankionline.com/balancer", "review9", "https://client-review-9-public.test-ru.tankionline.com/?config-template=https://c{server}.review-9-public.test-ru.tankionline.com/config.xml&resources=https://resources-review-9-public.test-ru.tankionline.com&balancer=https://balancer.review-9-public.test-ru.tankionline.com/balancer", "Client Review #9"],
-                    ["https://balancer.3dtank.com/balancer/",                             "3dtank",  "https://3dtank.com/play/", '3D<svg xmlns="http://www.w3.org/2000/svg" version="1.2" viewBox="0 0 14 8" fill="currentColor" style="height: 0.8em;display: inline;margin-top: 0.15em;"><path fill-rule="evenodd" d="m11.3 0c0.1 0 0.2 0.1 0.2 0.3v0.7h2.2c0.2 0 0.3 0.1 0.3 0.3v0.5c0 0.1-0.1 0.2-0.3 0.2h-2.2v1h2.2c0.2 0 0.3 0.1 0.3 0.3v2.5c0 0.1-0.1 0.2-0.3 0.2h-1.2v1h1.2c0.2 0 0.3 0.1 0.3 0.3v0.5c0 0.1-0.1 0.2-0.3 0.2h-2c-0.1 0-0.2-0.1-0.2-0.2v-1.8h-1v0.5c0 0.8-0.7 1.5-1.5 1.5h-0.8c-0.1 0-0.2-0.1-0.2-0.2v-0.5c0-0.2 0.1-0.3 0.2-0.3h0.8c0.3 0 0.5-0.2 0.5-0.5v-0.5h-1.3c-0.1 0-0.2-0.1-0.2-0.2v-2.5c0-0.2 0.1-0.3 0.2-0.3h2.3v-1h-2.3c-0.1 0-0.2-0.1-0.2-0.2v-0.5c0-0.2 0.1-0.3 0.2-0.3h2.3v-0.7c0-0.2 0.1-0.3 0.2-0.3zm1.8 5v-1h-4v1z"/><path fill-rule="evenodd" d="m3 7.3c0-0.2 0.1-0.3 0.3-0.3h3.5c0.1 0 0.2 0.1 0.2 0.3v0.5c0 0.1-0.1 0.2-0.3 0.2h-3.5c-0.1 0-0.2-0.1-0.2-0.2zm3.8-6.3c0.1 0 0.2 0.1 0.2 0.3v4.5c0 0.1-0.1 0.2-0.2 0.2h-3.5c-0.2 0-0.3-0.1-0.3-0.3v-4.5c0-0.1 0.1-0.2 0.3-0.2zm-0.8 4v-1h-2v1zm-2-2h2v-1h-2zm-2.8-2c0.2 0 0.3 0.1 0.3 0.3v0.7h0.3c0.1 0 0.2 0.1 0.2 0.3v0.5c0 0.1-0.1 0.2-0.2 0.2h-0.3v4h0.3c0.1 0 0.2 0.1 0.2 0.3v0.5c0 0.1-0.1 0.2-0.2 0.2h-1.5c-0.2 0-0.3-0.1-0.3-0.2v-0.5c0-0.2 0.1-0.3 0.3-0.3h0.2v-4h-0.2c-0.2 0-0.3-0.1-0.3-0.2v-0.5c0-0.2 0.1-0.3 0.3-0.3h0.2v-0.8c0-0.1 0.1-0.2 0.3-0.2z"/></svg>']];
+                    ["https://balancer.3dtank.com/balancer/",                             "3dtank",  "https://3dtank.com/play/", '3D<svg xmlns="http://www.w3.org/2000/svg" version="1.2" viewBox="0 0 14 8" fill="currentColor" style="height: 0.8em;display: inline;margin-top: 0.15em;"><path fill-rule="evenodd" d="m11.3 0c0.1 0 0.2 0.1 0.2 0.3v0.7h2.2c0.2 0 0.3 0.1 0.3 0.3v0.5c0 0.1-0.1 0.2-0.3 0.2h-2.2v1h2.2c0.2 0 0.3 0.1 0.3 0.3v2.5c0 0.1-0.1 0.2-0.3 0.2h-1.2v1h1.2c0.2 0 0.3 0.1 0.3 0.3v0.5c0 0.1-0.1 0.2-0.3 0.2h-2c-0.1 0-0.2-0.1-0.2-0.2v-1.8h-1v0.5c0 0.8-0.7 1.5-1.5 1.5h-0.8c-0.1 0-0.2-0.1-0.2-0.2v-0.5c0-0.2 0.1-0.3 0.2-0.3h0.8c0.3 0 0.5-0.2 0.5-0.5v-0.5h-1.3c-0.1 0-0.2-0.1-0.2-0.2v-2.5c0-0.2 0.1-0.3 0.2-0.3h2.3v-1h-2.3c-0.1 0-0.2-0.1-0.2-0.2v-0.5c0-0.2 0.1-0.3 0.2-0.3h2.3v-0.7c0-0.2 0.1-0.3 0.2-0.3zm1.8 5v-1h-4v1z"></path><path fill-rule="evenodd" d="m3 7.3c0-0.2 0.1-0.3 0.3-0.3h3.5c0.1 0 0.2 0.1 0.2 0.3v0.5c0 0.1-0.1 0.2-0.3 0.2h-3.5c-0.1 0-0.2-0.1-0.2-0.2zm3.8-6.3c0.1 0 0.2 0.1 0.2 0.3v4.5c0 0.1-0.1 0.2-0.2 0.2h-3.5c-0.2 0-0.3-0.1-0.3-0.3v-4.5c0-0.1 0.1-0.2 0.3-0.2zm-0.8 4v-1h-2v1zm-2-2h2v-1h-2zm-2.8-2c0.2 0 0.3 0.1 0.3 0.3v0.7h0.3c0.1 0 0.2 0.1 0.2 0.3v0.5c0 0.1-0.1 0.2-0.2 0.2h-0.3v4h0.3c0.1 0 0.2 0.1 0.2 0.3v0.5c0 0.1-0.1 0.2-0.2 0.2h-1.5c-0.2 0-0.3-0.1-0.3-0.2v-0.5c0-0.2 0.1-0.3 0.3-0.3h0.2v-4h-0.2c-0.2 0-0.3-0.1-0.3-0.2v-0.5c0-0.2 0.1-0.3 0.3-0.3h0.2v-0.8c0-0.1 0.1-0.2 0.3-0.2z"></path></svg>']];
 let statuses = [];
 serverdata.forEach(serveradats => {
     fetch(serveradats[0]).then(response => {
@@ -66,6 +66,10 @@ window.onload = (event) => {
     }
     mtb_accounts = localStorage.getItem("mtb_accounts")
     let accounts = JSON.parse(mtb_accounts);
+    const addacc = document.querySelector('div#accadd');
+    if (accounts[0] == "."){
+        addacc.setAttribute("style", "background-image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.2' viewBox='0 0 20 20' width='20' height='20'%3E%3Cpath d='m0 15.8v4.2h4.2l12.3-12.3-4.2-4.2zm19.7-11.3c0.4-0.4 0.4-1.1 0-1.6l-2.6-2.6c-0.5-0.4-1.2-0.4-1.6 0l-2 2.1 4.1 4.1z' fill='%23fff'/%3E%3C/svg%3E\");")
+    }
     const accselect = document.querySelector('div#accselect');
     const accselecttext = document.querySelector('div.select:nth-of-type(2)>span.selected');
     const fight = document.querySelector('a#play');
@@ -74,7 +78,7 @@ window.onload = (event) => {
     function pievienot (name) {
         let konts = document.createElement("span");
         if (name == "."){
-            konts.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" version="1.2" viewBox="0 0 404 100" width="404" height="100" style="margin-top: 0.125em;max-height: 1em;width: fit-content;"><style>.a{stroke:#fff;stroke-width: 0.5em;stroke-miterlimit:100;fill: #fff;}</style><path fill-rule="evenodd" class="a" d="m7.9 2q0 35.9 0 47.9 31.5 0 42.1 0 0-35.8 0-47.9c2.8 0 4.6 0 5.9 0.1 0 0 0.2 6.5 0.1 19.4-0.1 13 0 24.5 0 34.5q-36 0-48.1 0 0 27.9 0 42c0 0-4.7 0-5.5 0q0-71.9 0-96c2.8-0.1 4.7 0 5.5 0zm117.2 0q0 24.1 0 96c-9 0-27-0.1-54 0 0-7.5 0-22.5 0-45 8-0.1 23.9 0 48-0.1 0-7.5 0-22.3 0-44.9-8 0.1-24.2-0.1-48.2 0 0-0.9 0-3.1 0-6 0 0 27.2 0.1 54.2 0zm-48.1 57q0 8.2 0 33c7.1 0 21.1 0.1 42.1 0 0-5.5 0-16.6 0-33.1q-10.6 0-42.1 0.1zm81.2-4.9c0-2.3 0-5.9 0-7.9 13.5-18 24.7-32.2 29.1-38.2-22.5 0-39.7 0.1-47.2 0.1q0-4.6 0-6.1 36 0 48.1 0 3.9 4 6 6.1-22.6 31.4-30.1 41.8 22.6 31.5 30.1 42.1-4.6 4.5-6 6-36.1 0-48.1 0 0-4.5 0-6c22.6 0 39.8 0 47.3 0-13.6-17.9-29.2-37.9-29.2-37.9zm51-52.1c9.1 0 27.1 0.1 54 0q0 24.1 0 96c-8.9 0-27-0.1-54 0 0-7.5 0.1-22.6 0.1-45.1 8.1-0.1 23.9 0.1 47.9 0 0-7.5 0-22.3 0-44.8-8 0.1-24-0.1-48 0q0-1.5 0-5.9zm6 56.9c0 5.4 0 16.6 0 33.1 7 0 21 0.1 42 0 0-5.4 0-16.6 0-33.1-7 0-21-0.1-42 0zm63 33.1c9.1 0 18.2 0 26.6 0q12.7 0 21.6 0 0-30 0-90 1.5 0 5.9 0 0 24.1 0 96-13.5 0-54.1 0c0-2.5 0.1-3.9 0-6zm123.2-90q0 24.1 0 96c-9 0-27.1-0.2-54.1-0.1 0-7.5 0-22.4 0-44.9 8.1-0.1 24 0.2 48 0.1 0-7.6 0-22.6 0-45.1-8 0.1-24 0-48 0 0-0.9 0-3 0-6 0 0 27.1 0.1 54.1 0zm-48.1 57q0 8.2 0 33c7.1 0 21 0.2 42 0.1 0-5.5 0.1-16.5 0.1-33-7 0-21.1-0.2-42.1-0.1z"></path></svg>';
+            konts.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" version="1.2" viewBox="0 0 1056 213" fill="currentColor" style="height: 1em;display: inline;"><path fill-rule="evenodd" d="m1014.9 178v-55.7h-67.1v55.7zm0-89.1v-53.9h-100.1q-3.3 0-5.4-2.1-2.2-2.2-2.2-5.2v-20.1q0-3.3 2.2-5.5 2.1-2.1 5.4-2.1h133.9q3.1 0 5.2 2.1 2.1 2.2 2.1 5.5v198.1q0 3-2.1 5.2-2.1 2.1-5.2 2.1h-136.7q-3.1 0-5.2-2.1-2.1-2.2-2.1-5.2v-109.5q0-3 2.1-5.1c1.4-1.5 3.2-2.2 5.2-2.2zm-282.3 122q-2.1-2.1-2.1-5.2v-21.3q0-3.3 2.1-5.4 2.1-2.2 5.5-2.2h96.3v-169.3q0-3 2.2-5.2 2.1-2.1 5.1-2.1h28.3q3 0 5.2 2.1 2.1 2.2 2.1 5.2v198.2q0 3.1-2.1 5.2-2.2 2.1-5.2 2.1h-131.9q-3.4 0-5.5-2.1zm-69.7-32.9v-55.7h-67v55.7zm0-89.1v-53.9h-100.2q-3.3 0-5.4-2.1-2.2-2.2-2.2-5.2v-20.1q0-3.3 2.2-5.5 2.1-2.1 5.4-2.1h133.9q3.1 0 5.2 2.1 2.1 2.2 2.1 5.5v198.1q0 3-2.1 5.2-2.1 2.1-5.2 2.1h-136.7q-3 0-5.2-2.1-2.1-2.2-2.1-5.2v-109.5q0-3 2.1-5.1c1.5-1.5 3.2-2.2 5.2-2.2zm-281.1 121.5q-2.2-1.6-2.2-4v-19q0-2.4 2.2-4 2.1-1.7 5.1-1.7h81.5l-82.1-62.8q-2.7-1.9-4.5-3.7-2.2-2.2-2.2-5v-8q0-2.8 2.2-4.7 1.8-2.1 4.5-4l82.1-62.8h-81.5q-3 0-5.1-1.6-2.2-1.7-2.2-4v-19.1q0-2.3 2.2-4 2.1-1.6 5.1-1.6h131.4q3 0 5.1 1.6 2.2 1.7 2.2 4v17.2q0 2.8-1.9 4.9-1.8 1.9-4.5 4l-95.2 72.9v2.4l95.2 72.9q2.7 2.1 4.5 4 1.9 1.9 1.9 4.9v17.2q0 2.4-2.2 4-2.1 1.7-5.1 1.7h-131.4q-3 0-5.1-1.7zm-69.9-32.4v-55.7h-67v55.7zm0-89.1v-53.9h-100.1q-3.4 0-5.5-2.1-2.1-2.2-2.1-5.2v-20.1q0-3.3 2.1-5.5 2.1-2.1 5.5-2.1h133.8q3.1 0 5.2 2.1 2.2 2.2 2.2 5.5v198.1q0 3.1-2.2 5.2-2.1 2.1-5.2 2.1h-136.7q-3 0-5.1-2.1-2.2-2.1-2.2-5.2v-109.5q0-3 2.2-5.1c1.4-1.5 3.1-2.2 5.1-2.2zm-268.7 35.1v81.4q0 3.1-2.2 5.2-1.8 2.1-5.1 2.1h-27.7q-3 0-5.1-1.8-2.2-2.1-2.2-5.5v-197.8q0-3.3 2.2-5.4 2.1-2.2 5.1-2.2h27.7q3.3 0 5.1 2.2 2.2 2.1 2.2 5.4v78.4h89.6v-78.4q0-3.3 2.1-5.4 2.2-2.2 5.2-2.2h27.3q3.4 0 5.5 2.2 2.1 2.1 2.1 5.4c0 0 0 107.4 0 108.9 0 2.1-6 7.5-8.4 7.5-1.6 0-123.4 0-123.4 0z"/></svg>';
         } else{
             konts.innerHTML = name;
         }
@@ -86,9 +90,9 @@ window.onload = (event) => {
             saite = "https://"+izvelets+".tankionline.com/play/"
             if (izvelets == "."){
                 fight.setAttribute('href', "https://tankionline.com/play/");
-                accselecttext.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" version="1.2" viewBox="0 0 404 100" width="404" height="100" style="margin-top: 0.125em;max-height: 1em;width: fit-content;"><style>.a{stroke:#fff;stroke-width: 0.5em;stroke-miterlimit:100;fill: #fff;}</style><path fill-rule="evenodd" class="a" d="m7.9 2q0 35.9 0 47.9 31.5 0 42.1 0 0-35.8 0-47.9c2.8 0 4.6 0 5.9 0.1 0 0 0.2 6.5 0.1 19.4-0.1 13 0 24.5 0 34.5q-36 0-48.1 0 0 27.9 0 42c0 0-4.7 0-5.5 0q0-71.9 0-96c2.8-0.1 4.7 0 5.5 0zm117.2 0q0 24.1 0 96c-9 0-27-0.1-54 0 0-7.5 0-22.5 0-45 8-0.1 23.9 0 48-0.1 0-7.5 0-22.3 0-44.9-8 0.1-24.2-0.1-48.2 0 0-0.9 0-3.1 0-6 0 0 27.2 0.1 54.2 0zm-48.1 57q0 8.2 0 33c7.1 0 21.1 0.1 42.1 0 0-5.5 0-16.6 0-33.1q-10.6 0-42.1 0.1zm81.2-4.9c0-2.3 0-5.9 0-7.9 13.5-18 24.7-32.2 29.1-38.2-22.5 0-39.7 0.1-47.2 0.1q0-4.6 0-6.1 36 0 48.1 0 3.9 4 6 6.1-22.6 31.4-30.1 41.8 22.6 31.5 30.1 42.1-4.6 4.5-6 6-36.1 0-48.1 0 0-4.5 0-6c22.6 0 39.8 0 47.3 0-13.6-17.9-29.2-37.9-29.2-37.9zm51-52.1c9.1 0 27.1 0.1 54 0q0 24.1 0 96c-8.9 0-27-0.1-54 0 0-7.5 0.1-22.6 0.1-45.1 8.1-0.1 23.9 0.1 47.9 0 0-7.5 0-22.3 0-44.8-8 0.1-24-0.1-48 0q0-1.5 0-5.9zm6 56.9c0 5.4 0 16.6 0 33.1 7 0 21 0.1 42 0 0-5.4 0-16.6 0-33.1-7 0-21-0.1-42 0zm63 33.1c9.1 0 18.2 0 26.6 0q12.7 0 21.6 0 0-30 0-90 1.5 0 5.9 0 0 24.1 0 96-13.5 0-54.1 0c0-2.5 0.1-3.9 0-6zm123.2-90q0 24.1 0 96c-9 0-27.1-0.2-54.1-0.1 0-7.5 0-22.4 0-44.9 8.1-0.1 24 0.2 48 0.1 0-7.6 0-22.6 0-45.1-8 0.1-24 0-48 0 0-0.9 0-3 0-6 0 0 27.1 0.1 54.1 0zm-48.1 57q0 8.2 0 33c7.1 0 21 0.2 42 0.1 0-5.5 0.1-16.5 0.1-33-7 0-21.1-0.2-42.1-0.1z"></path></svg>';
+                accselecttext.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" version="1.2" viewBox="0 0 1056 213" fill="currentColor" style="height: 1em;display: inline;margin: auto 0;"><path fill-rule="evenodd" d="m1014.9 178v-55.7h-67.1v55.7zm0-89.1v-53.9h-100.1q-3.3 0-5.4-2.1-2.2-2.2-2.2-5.2v-20.1q0-3.3 2.2-5.5 2.1-2.1 5.4-2.1h133.9q3.1 0 5.2 2.1 2.1 2.2 2.1 5.5v198.1q0 3-2.1 5.2-2.1 2.1-5.2 2.1h-136.7q-3.1 0-5.2-2.1-2.1-2.2-2.1-5.2v-109.5q0-3 2.1-5.1c1.4-1.5 3.2-2.2 5.2-2.2zm-282.3 122q-2.1-2.1-2.1-5.2v-21.3q0-3.3 2.1-5.4 2.1-2.2 5.5-2.2h96.3v-169.3q0-3 2.2-5.2 2.1-2.1 5.1-2.1h28.3q3 0 5.2 2.1 2.1 2.2 2.1 5.2v198.2q0 3.1-2.1 5.2-2.2 2.1-5.2 2.1h-131.9q-3.4 0-5.5-2.1zm-69.7-32.9v-55.7h-67v55.7zm0-89.1v-53.9h-100.2q-3.3 0-5.4-2.1-2.2-2.2-2.2-5.2v-20.1q0-3.3 2.2-5.5 2.1-2.1 5.4-2.1h133.9q3.1 0 5.2 2.1 2.1 2.2 2.1 5.5v198.1q0 3-2.1 5.2-2.1 2.1-5.2 2.1h-136.7q-3 0-5.2-2.1-2.1-2.2-2.1-5.2v-109.5q0-3 2.1-5.1c1.5-1.5 3.2-2.2 5.2-2.2zm-281.1 121.5q-2.2-1.6-2.2-4v-19q0-2.4 2.2-4 2.1-1.7 5.1-1.7h81.5l-82.1-62.8q-2.7-1.9-4.5-3.7-2.2-2.2-2.2-5v-8q0-2.8 2.2-4.7 1.8-2.1 4.5-4l82.1-62.8h-81.5q-3 0-5.1-1.6-2.2-1.7-2.2-4v-19.1q0-2.3 2.2-4 2.1-1.6 5.1-1.6h131.4q3 0 5.1 1.6 2.2 1.7 2.2 4v17.2q0 2.8-1.9 4.9-1.8 1.9-4.5 4l-95.2 72.9v2.4l95.2 72.9q2.7 2.1 4.5 4 1.9 1.9 1.9 4.9v17.2q0 2.4-2.2 4-2.1 1.7-5.1 1.7h-131.4q-3 0-5.1-1.7zm-69.9-32.4v-55.7h-67v55.7zm0-89.1v-53.9h-100.1q-3.4 0-5.5-2.1-2.1-2.2-2.1-5.2v-20.1q0-3.3 2.1-5.5 2.1-2.1 5.5-2.1h133.8q3.1 0 5.2 2.1 2.2 2.2 2.2 5.5v198.1q0 3.1-2.2 5.2-2.1 2.1-5.2 2.1h-136.7q-3 0-5.1-2.1-2.2-2.1-2.2-5.2v-109.5q0-3 2.2-5.1c1.4-1.5 3.1-2.2 5.1-2.2zm-268.7 35.1v81.4q0 3.1-2.2 5.2-1.8 2.1-5.1 2.1h-27.7q-3 0-5.1-1.8-2.2-2.1-2.2-5.5v-197.8q0-3.3 2.2-5.4 2.1-2.2 5.1-2.2h27.7q3.3 0 5.1 2.2 2.2 2.1 2.2 5.4v78.4h89.6v-78.4q0-3.3 2.1-5.4 2.2-2.2 5.2-2.2h27.3q3.4 0 5.5 2.2 2.1 2.1 2.1 5.4c0 0 0 107.4 0 108.9 0 2.1-6 7.5-8.4 7.5-1.6 0-123.4 0-123.4 0z"/></svg>';
             }
-            else if (izvelets == accounts[0]) {
+            else if (izvelets == accounts[0].toLowerCase()) {
                 fight.setAttribute('href', "https://tankionline.com/play/");
                 accselecttext.innerHTML = name;
             } else {
@@ -100,27 +104,29 @@ window.onload = (event) => {
     accounts.forEach(pievienot)
     const preloader = document.querySelector('.preloader');
     preloader.setAttribute('style', 'animation-play-state: running;');
-    const addacc = document.querySelector('div#accadd');
     const errorlist = document.querySelector("div.errors");
     const inputacc = document.querySelector('input.accinput');
     inputacc.addEventListener('input', function() {
-        this.value = this.value.replace(/[^\x20-\x7E\u0080-\u02AF\u0400-\u04FF\u0590-\u05FF\u0600-\u06FF\u0530-\u058F0-9_ ]/g, '');
+        if (this.value == " "){this.value = "";} // remove first space
+        this.value = this.value.replace(/[^\x20-\x7E\u0080-\u02AF\u0400-\u04FF\u0590-\u05FF\u0600-\u06FF\u0530-\u058F0-9_ ]/g, '').replace(".", " ");
     });
+    function kluda(header, text, klase){
+        console.log(header, text)
+        let error = document.createElement("div");
+        error.classList.add(klase);
+        error.innerHTML = "<h2>" + header + "</h2><p>" + text + "</p>";
+        errorlist.appendChild(error);
+    }
     addacc.addEventListener('click', () => {
-        if (inputacc.value != ""){
-            if (['.', 'game', 'space', 'test', 'new-ru', 'new-en', 'new-de', 'new-pl', 'pages', 'ratings', 'help', 'public-deploy1.test-eu', 'public-deploy2.test-eu', 'public-deploy3.test-eu', 'public-deploy4.test-eu', 'public-deploy5.test-eu', 'public-deploy6.test-eu', 'public-deploy7.test-eu', 'public-deploy8.test-eu', 'public-deploy9.test-eu', 'client-review-1-public.test-ru', 'client-review-2-public.test-ru', 'client-review-3-public.test-ru', 'client-review-4-public.test-ru', 'client-review-5-public.test-ru', 'client-review-6-public.test-ru', 'client-review-7-public.test-ru', 'client-review-8-public.test-ru', 'client-review-9-public.test-ru', 's.eu', 'helpdesk.eu'].includes(inputacc.value)){
-                console.log("Invalid name. Try another")
-                let error = document.createElement("div");
-                error.classList.add("error");
-                error.innerHTML = "<h2>Error</h2><p>Invalid name. Try another</p>";
-                errorlist.appendChild(error);
+        if (inputacc.value == "" && accounts[0] == "." && inputacc.getAttribute("class") == "accinput"){
+            kluda("Pay attention!", "First time you add profile, you actually add name to the first (general) profile. After renaming you can add more profiles.", "warning")
+        } else if (inputacc.value != ""){
+            if (['.', 'game', 'space', 'test', 'new-ru', 'new-en', 'new-de', 'new-pl', 'pages', 'ratings', 'help', 'public-deploy1.test-eu', 'public-deploy2.test-eu', 'public-deploy3.test-eu', 'public-deploy4.test-eu', 'public-deploy5.test-eu', 'public-deploy6.test-eu', 'public-deploy7.test-eu', 'public-deploy8.test-eu', 'public-deploy9.test-eu', 'client-review-1-public.test-ru', 'client-review-2-public.test-ru', 'client-review-3-public.test-ru', 'client-review-4-public.test-ru', 'client-review-5-public.test-ru', 'client-review-6-public.test-ru', 'client-review-7-public.test-ru', 'client-review-8-public.test-ru', 'client-review-9-public.test-ru', 's.eu', 'helpdesk.eu', 'auth', 'war', 'event', 'helper', 'shop', 'mail'].includes(inputacc.value)){
+                kluda("Error", "Invalid name. Try another", "error");
             } else{
-                console.log("else")
                 if (JSON.stringify(accounts) == JSON.stringify(['.'])){
-                    console.log("else if")
                     accounts = '["'+inputacc.value+'"]';
                     localStorage.setItem("mtb_accounts", accounts);
-                    console.log(document.querySelector('span[name="."]'))
                     document.querySelector('span[name="."]').remove();
                     accselecttext.innerHTML = inputacc.value;
                 } else {
@@ -128,6 +134,7 @@ window.onload = (event) => {
                     localStorage.setItem("mtb_accounts", JSON.stringify(accounts));
                 }
                 pievienot(inputacc.value);
+                addacc.setAttribute("style", "")
             }
             inputacc.value = "";
         }
@@ -150,7 +157,7 @@ window.onload = (event) => {
                 accselecttext.innerHTML = "Available only for main servers";
             } else{
                 serverlist.classList.add("selected")
-                accselecttext.innerHTML = "Choose the account";
+                accselecttext.innerHTML = "Choose your profile";
                 if (izvelets == "main"){
                     fight.setAttribute('href', "https://tankionline.com/play/");
                 }
